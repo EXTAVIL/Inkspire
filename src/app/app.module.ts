b651a54f8717978f51c3172d8f4440012c56d1ee
sujithsojan <sujithsojan@gmail.com>
2025-03-27 15:15:19 +0530

    Working - The UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PostCreatePage } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { PostListPage } from './posts/post-list/post-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreatePage
-
+    PostCreatePage,
+    HeaderComponent,
+    PostListPage
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MatInputModule,
     MatCardModule,
-
-
+    MatToolbarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
